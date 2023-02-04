afbc3bc98bfd73c8f24481089de32bf65e36af61
faizbyp <faizbyp@gmail.com>
2023-02-04 09:04:48 +0700

    init commit

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1675476203,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/chara-sheet0.png",
+		"images/water-sheet0.png",
+		"images/waterspawner-sheet0.png",
+		"images/rock-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
